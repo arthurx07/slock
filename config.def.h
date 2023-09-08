2ab210bedfcec6d7fc40847732c0d050b5942655
kumao <arturleivarx@gmail.com>
2023-09-08 10:58:32 +0200

    apply 02-slock-git-20161012-control-clear.diff to slock-1.5

--- config.def.h
+++ config.def.h
@@ -13,3 +13,7 @@ static const int failonclear = 1;
 
 /* Background image path, should be available to the user above */
 static const char* background_image = "";
+
+
+/* allow control key to trigger fail on clear */
+static const int controlkeyclear = 0;
